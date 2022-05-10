d37edde4be6cc8647d03cf35597fbbfef3a1a489
Alexander Schau <alexander@schau.email>
2022-05-10 07:34:51 +0000

    some updates to landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import node from "@astrojs/node";
+import deno from "@astrojs/deno";
 
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
@@ -8,5 +9,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  adapter: node(),
+  adapter: deno(),
 });
